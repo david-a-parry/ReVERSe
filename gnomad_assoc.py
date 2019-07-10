5cf68adca04018ec0ffb8bc12fd6e806802cd2f4
David A. Parry <david.parry@igmm.ed.ac.uk>
2019-07-10 15:09:18 +0100

    fix logging

--- gnomad_assoc.py
+++ gnomad_assoc.py
@@ -23,6 +23,7 @@ ch.setLevel(logging.INFO)
 formatter = logging.Formatter(
        '[%(asctime)s] %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
+logger.addHandler(ch)
 prog_string = ''
 g_columns = ['alt', 'non_alt', 'P', 'OR',]
 
