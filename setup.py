19ba7d40bad6be1790044bff6c1f527dc2ee6ea2
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-05-05 15:17:17 +0100

    update installation requirements

--- setup.py
+++ setup.py
@@ -4,25 +4,25 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name = "ReVERSe",
-    packages = ["ReVERSe"],
-    version = "0.1",
-    description = "Rare Variant Enrichment and Recessive Segregation",
-    author = "David A. Parry",
-    author_email = "david.parry@igmm.ed.ac.uk",
-    url = "https://git.ecdf.ed.ac.uk/dparry/reverse",
+    name="ReVERSe",
+    packages=["ReVERSe"],
+    version="0.1",
+    description="Rare Variant Enrichment and Recessive Segregation",
+    author="David A. Parry",
+    author_email="david.parry@igmm.ed.ac.uk",
+    url='https://github.com/david-a-parry/ReVERSe',
+    download_url='https://github.com/david-a-parry/ReVERSe/archive/0.1.tar.gz',
     license='MIT',
-    install_requires=['vase>=0.2.4'],
+    install_requires=['vase>=0.2.5', 'pandas', 'xlsxwriter', 'requests'
+                      'biopython'],
     extras_require={
-        'BGZIP': ['biopython'],
-        'REPORTER': ['xlsxwriter', 'requests'],
         'MYGENEINFO': ['mygene'],
     },
-    scripts = ["bin/ReVERSe_count.py",
-               "bin/ReVERSe_reporter.py",
-               "bin/ReVERSe_seg.py"],
+    scripts=["bin/ReVERSe_count.py",
+             "bin/ReVERSe_reporter.py",
+             "bin/ReVERSe_seg.py"],
     include_package_data=True,
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -30,5 +30,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         ],
-    dependency_links=['https://github.com/david-a-parry/vase/tarball/master#egg=vase-0.2.4',],
+    dependency_links=[
+        'https://github.com/david-a-parry/vase/tarball/master#egg=vase-0.2.5'],
 )
