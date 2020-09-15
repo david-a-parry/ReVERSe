195b0db2b20694807583ad2e9d3cf57be063d2ee
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-09-15 17:53:11 +0100

    tidy up test directory

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-VCF=$DIR/test_in.vcf.gz
-PED=$DIR/test.ped
+VCF=$DIR/test_data/test_in.vcf.gz
+PED=$DIR/test_data/test.ped
 OUTDIR=$DIR/test_output
 mkdir -p $OUTDIR
 PREFIX=${OUTDIR}/test_output
@@ -12,10 +12,10 @@ set -euo pipefail
 echo $(date) Running ReVERSe_count
 ReVERSe_count -i $VCF \
     --ped $PED \
-    -e $DIR/test_gnomad_wes.vcf.gz \
-    -g $DIR/test_gnomad_wgs.vcf.gz \
-    --exome_coverage_files $DIR/test_wes_cov.tsv.gz \
-    --genome_coverage_files $DIR/test_wgs_cov.tsv.gz \
+    -e $DIR/test_data/test_gnomad_wes.vcf.gz \
+    -g $DIR/test_data/test_gnomad_wgs.vcf.gz \
+    --exome_coverage_files $DIR/test_data/test_wes_cov.tsv.gz \
+    --genome_coverage_files $DIR/test_data/test_wgs_cov.tsv.gz \
     --gnomad_version 2.1 \
     -v $PREFIX.rev_counts.vcf.gz \
     -t $PREFIX.rev_counts.txt.gz 
